d9fd85d62c1c1f12ebc5eafa39ffb82a65eda1e8
Tasnuva12 <tasnuvamehnaz@iut-dhaka.edu>
2024-03-26 13:50:12 +0600

    This is a single static page by React JS

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="/react.svg" alt="React Logo"  className="nav--icon" />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
